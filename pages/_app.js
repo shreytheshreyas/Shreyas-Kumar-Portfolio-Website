826723cb997c79dc1c8f1370ad799d528db5195c
shreytheshreyas <59922045+shreytheshreyas@users.noreply.github.com>
2024-12-22 01:55:40 +0800

    potential solve for dark theme issue

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true}>
       <Component {...pageProps} />
     </ThemeProvider>
   );
